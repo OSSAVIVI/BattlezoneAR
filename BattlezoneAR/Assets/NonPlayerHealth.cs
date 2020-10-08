2e0735bd002385f9a53b97ec6e7e624b4932ffc1
Ryan Farran <rwfarran@gmail.com>
2020-10-07 22:17:57 -0700

    attempting to add project

---
+++ BattlezoneAR/Assets/NonPlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayerHealth : MonoBehaviour
+{
+    public int health;
+    private float hitRate;
+    private float nextHit;
+
+    void Start()
+    {
+        hitRate = 0.1f;
+        nextHit = -1f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Time.time > nextHit && collision.gameObject.CompareTag("PlayerShot"))
+        {
+            print("PREDAMAGE: Non-Player Heatlh -> " + health);
+            nextHit = Time.time + hitRate;
+            health--;
+            print("POSTDAMAGE: Non-Player Heatlh -> " + health);
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ BattlezoneAR/Assets/PlayerShooting.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    bool isShooting;
+    public Rigidbody[] projectile;
+    public Transform shotSpawn;
+    private float fireRate;
+    private float nextFire;
+    private float shootForce;
+
+    void Start()
+    {
+        isShooting = false;
+        fireRate = 0.75f;
+        nextFire = -1f;
+        shootForce = 1500f;
+    }
+
+    void FixedUpdate()
+    {
+        if (isShooting)
+        {
+            Shoot();
+        }
+    }
+
+    public void ToggleAttackOn()
+    {
+        print("FIRE BUTTON DOWN");
+        isShooting = true;
+    }
+
+    public void ToggleAttackOff()
+    {
+        print("FIRE BUTTON UP");
+        isShooting = false;
+    }
+
+    public void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            print("FIRING SHOT!");
+            nextFire = Time.time + fireRate;
+            Rigidbody shot = Instantiate(projectile[0], shotSpawn.position, shotSpawn.rotation);
+            shot.AddForce(shotSpawn.forward * shootForce);
+        }
+    }
+}
