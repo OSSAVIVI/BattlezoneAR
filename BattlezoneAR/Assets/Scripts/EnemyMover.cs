fc8e0beef98e27e75f97daeebddcb219c6b30757
moragaj <35177734+moragaj@users.noreply.github.com>
2020-10-18 18:49:57 -0500

    Added tanks, enemy mover script, and spawn enemy script

---
+++ BattlezoneAR/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+	//public GameObject player;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 1 * Time.deltaTime);
+        transform.position += transform.forward * 1f * Time.deltaTime;
+    }
+}
