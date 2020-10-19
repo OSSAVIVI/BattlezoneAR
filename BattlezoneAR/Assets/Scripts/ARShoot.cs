dcbfc245222bbc63e412a071a71eb4127effe737
Bryce Hickson <45865175+hicksonb@users.noreply.github.com>
2020-10-19 12:38:48 -0500

    AR shooting enabled, Created multiple spawn points and occurences

---
+++ BattlezoneAR/Assets/Scripts/ARShoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARShoot : MonoBehaviour
+{
+    public Rigidbody[] projectile;
+    public Transform shotSpawn;
+
+    public void arShoot()
+    {
+        Rigidbody shot = Instantiate(projectile[0], shotSpawn.position, shotSpawn.rotation);
+        shot.AddForce(shotSpawn.forward * 1500f);
+    }
+}
--- BattlezoneAR/Assets/Scripts/SpawnEnemy.cs
+++ BattlezoneAR/Assets/Scripts/SpawnEnemy.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading;
 using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    public GameObject newTank;
-    //public Vector3 spawnSpot = new Vector3(5,5,5);
-    public Vector3 spawnSpot;
+    //public GameObject newTank;
+    ////public Vector3 spawnSpot = new Vector3(5,5,5);
+    //public Vector3 spawnSpot;
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemyTank;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnSpot = new Vector3(15,0,15);
-        GameObject player = GameObject.FindWithTag("Player");
-        Instantiate(newTank, spawnSpot, transform.rotation);
+        //spawnSpot = new Vector3(15,0,15);
+        //GameObject player = GameObject.FindWithTag("Player");
+        //Instantiate(newTank, spawnSpot, transform.rotation);
+
+        StartCoroutine(PrimaryEnemySpawn());
+    }
+
+    IEnumerator PrimaryEnemySpawn()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyTank[i], spawnPoints[i].position, Quaternion.identity);
+            yield return new WaitForSeconds(7);
+        }
+
+        StartCoroutine(PrimaryEnemySpawn());
     }
 
     // Update is called once per frame
