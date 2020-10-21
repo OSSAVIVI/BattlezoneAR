688217a0b04327a2341ed9a37567c72be623393c
kimjasmi <72484550+kimjasmi@users.noreply.github.com>
2020-10-20 23:28:11 -0700

    Main Menu buttons functionality

---
+++ BattlezoneAR/Assets/Scripts/UIscripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject helpMenu;
+
+
+    public void help() {
+        mainMenu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    public void back(){
+        mainMenu.SetActive(true);
+        helpMenu.SetActive(false);
+    }
+}
+
