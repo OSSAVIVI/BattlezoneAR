5eb99616406edebef391ef94ff466b03de2e4f63
moragaj <35177734+moragaj@users.noreply.github.com>
2020-11-02 16:59:28 -0600

    Removed gravity for enemy and player shot

---
+++ BattlezoneAR/Assets/Scripts/DestroyDelay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDelay : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+}
