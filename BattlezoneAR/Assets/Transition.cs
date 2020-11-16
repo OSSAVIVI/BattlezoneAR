12b5644bd0b031f2e1bf5ac3f9e53f4493fdb202
kimjasmi <72484550+kimjasmi@users.noreply.github.com>
2020-11-15 20:32:33 -0800

    Add wait time to Game Over Screen

---
+++ BattlezoneAR/Assets/Transition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        StartCoroutine(ChangeScreen2Seconds());
+
+    }
+
+   IEnumerator ChangeScreen2Seconds()
+    {
+        
+        yield return new WaitForSeconds(2);
+        
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+   
+}
